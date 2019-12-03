ae82363cebcd8fcae7e6ea4a21f4c1d7af227b8c
ilyayelagov <ilyayelagov@ILYAYELAGOV9C32>
2019-12-03 19:09:53 +0300

    Add project files.

---
+++ Lab 4 Test Generator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4_Test_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get file path
+            //Load
+
+        }
+    }
+}
---
+++ TestGeneratorLib/TestGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestGeneratorLib
+{
+    public class TestGenerator
+    {
+        public void GenerateForAtPath(? input, ? output)
+        {
+
+        }
+        public TestGenerator()
+        {
+
+        }
+    }
+}
